432f064c7246bdfd22777e2d36451f22e98e7f5b
xljutopia <xielijiautopia@gmail.com>
2017-07-12 16:56:22 +0800

    use basicbolt instead, and cancel writing file

--- src/main/java/com/abc/cpqs/bolt/PromotionBolt.java
+++ src/main/java/com/abc/cpqs/bolt/PromotionBolt.java
@@ -1,44 +1,19 @@
 package com.abc.cpqs.bolt;
 
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
 import com.abc.cpqs.dao.UserDAO;
 import com.abc.cpqs.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Map;
-
 /**
  * Created by lijiax on 6/13/17.
  */
-public class PromotionBolt extends BaseRichBolt {
+public class PromotionBolt extends BaseBasicBolt {
     private static Logger logger = LoggerFactory.getLogger(PromotionBolt.class);
-
-    FileOutputStream fop = null;
-    File file = null;
-
-    @Override
-    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
-        file = new File("/home/lijiax/logs/storm_out.txt");
-        try {
-            fop = new FileOutputStream(file);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
@@ -46,41 +21,20 @@ public class PromotionBolt extends BaseRichBolt {
     }
 
     @Override
-    public void execute(Tuple tuple) {
+    public void execute(Tuple tuple,BasicOutputCollector collector) {
         String out = tuple.getString(0).trim();
-        logger.info("bolt got tuple " + out);
-        User user = UserDAO.getUser(Integer.parseInt(out) * 10);
-        //logger.info("query db result is "+user);
-        /*Properties conf = PropertyLoader.getProperties("/home/lijiax/conf/socket.properties");
-        String ip = conf.getProperty("promotion.ip");
-        int port = Integer.parseInt(conf.getProperty("promotion.port"));
-        SocketClient client = new SocketClient(ip, port);*/
-        String userStr = null;
+        int id = Integer.parseInt(out) * 10;
+        logger.info("Bolt got a tuple " + out);
+        User user = UserDAO.getUser(id);
+
+        String userStr;
         if (null != user) {
-            userStr = user.toString() + "\n";
-            logger.info("query db result is " + userStr);
+            userStr = "query id is "+id+" and "+user.toString();
+            logger.info(userStr);
         } else {
-            userStr = "no result \n";
-            logger.info("query result is empty");
-        }
-        byte[] contentInBytes = userStr.getBytes();
-        try {
-            fop.write(contentInBytes);
-            fop.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+            //userStr = "query id is "+id+" no result \n";
+            logger.info("query id is "+id+" and result is empty.");
         }
     }
 
-    @Override
-    public void cleanup() {
-        super.cleanup();
-        try {
-            if (fop != null) {
-                fop.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
